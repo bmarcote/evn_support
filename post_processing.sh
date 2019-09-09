6bf8a54502c1b96e8527b1de4dcf598abb1c1c8b
Benito <marcote@jive.eu>
2019-09-09 11:56:10 +0200

    Fixed input parameters

--- post_processing.sh
+++ post_processing.sh
@@ -115,7 +115,7 @@ function post_process_pipe() {
     if [[ ! ( -n $1 && -n $2 ) ]];then
         echo "Two parameters are required:"
         echo " - experiment name (case insensitive)."
-        echo " - session (in mmmYY format e.g. feb18)."
+        echo " - Observing date (in mmmYY format e.g. feb18)."
         exit
     fi
 
@@ -188,7 +188,7 @@ function post_process_pipe() {
 
 if [[ "$(hostname)" = "eee2" ]];then
     echo "We are in eee2!"
-    post_process_eee $1 $2
+    post_process_eee $1 $2 $3
 elif [[ "$(hostname)" = "jop83" ]];then
     echo "We are in pipe!"
     post_process_pipe $1 $2
@@ -200,7 +200,7 @@ else
         echo " - experiment name (case insensitive)."
         echo " - Reference station to be used for the plots and pipeline."
         echo " - Calibrators to be used for the standardplots."
-        echo " - session (in mmmYY format e.g. feb18)."
+        echo " - Observing date (in mmmYY format e.g. jan18)."
         exit
     fi
     # Three parameters are expected:
@@ -208,8 +208,8 @@ else
     #	- The reference station to use in standardplots.
     #	- The calibrators to use in standardplots.
     #	- The session (in mmmYY format e.g. feb18)
-    ssh jops@eee "post_process_eee $1 $2 $3"
-    ssh pipe@jop83 "post_process_pipe $1 $4"
+    ssh jops@eee -t "HOME=/data0/marcote/;zsh" "post_processing.sh $1 $2 $3"
+    ssh pipe@jop83 -t "setenv HOME /jop83_0/pipe/in/marcote;post_processing.sh $1 $4"
     ssh jops@jop83 "archive_pipeline ${exp} ${date}"
     echo '\n\nWork finished. You may want to distribute the experiment!\n'
 fi
